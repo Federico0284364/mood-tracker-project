87ff1656bc95f376d1cf4e5e7daeff20adf6e709
Federico0284364 <tabella.federico@gmail.com>
2025-06-10 13:08:40 +0200

    reso h1 e h2 nel mood modal semibold

--- src/components/ModalContent.jsx
+++ src/components/ModalContent.jsx
@@ -3,8 +3,8 @@ import OptionBar from "./UI/OptionBar";
 export default function ModalContent({title, subtitle, numberOfPages, pageNumber, children}) {
 	return (
 		<>
-			<h1 className="text-2xl">{title}</h1>
-			<h2 className="text-xl">{subtitle}</h2>
+			<h1 className="text-2xl font-semibold">{title}</h1>
+			<h2 className="text-xl font-semibold">{subtitle}</h2>
 			<OptionBar numberOfElements={numberOfPages} index={pageNumber} />
 			{children}
 		</>
